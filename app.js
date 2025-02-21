bac93dcf77aa6e12e3a427721058b0012a9dbd76
Marinajwb <byun8688@gmail.com>
2025-02-21 22:56:46 +0900

    에러핸들링

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require("path");
 const express = require('express');
-
+const uuid = require('uuid');
 const app = express();
 app.set('views',path.join(__dirname,'views'));
 //처리하려는 템플릿 파일의 위치를 express에게 알려주어야함
@@ -30,17 +30,34 @@ app.get('/restaurants',function(req,res){
 
 app.get('/restaurants/:id',function(req,res){
     const restaurantId = req.params.id;
-    res.render('restaurants-detail',{rid : restaurantId})
-    //params.뒤에 오는값은 내가 콜론뒤에 설정해준 값이름으로 
+    const filePath = path.join(__dirname,'./data/restaurants.json')
+    const fileData  = fs.readFileSync(filePath)
+    const storedRestaurants = JSON.parse(fileData);
+    
+    for(const restaurant of storedRestaurants){
+       if(restaurant.id === restaurantId) {
+       return res.render('restaurants-detail',{restaurant : restaurant})
+        //params.뒤에 오는값은 내가 콜론뒤에 설정해준 값이름으로 
+        //중첩되지 않은 폴더의 경로로 되어있을 때는 css스타일의 경로를 상대적으로 지정해도
+        //작동했지만 이제 아이디값이 동적으로 더 붙기떄문에 절대경로로 잡아주어야함 
+    }
+    //라우팅을 하고 응답해주지않으면 time out 발생
+    res.status(404).render('404');
+}
+   
+    
 });
 //:id값 형식으로 동적 라우팅
 
 app.get('/recommend',function(req,res){
     res.render('recommend');
+    
  });
 
  app.post('/recommend', function (req,res){
     const restaurant = req.body
+    restaurant.id = uuid.v4();
+    // 객체로 들어오는 값이 아닌 내가 임의로 지정해준 값 
     const filePath = path.join(__dirname,'./data/restaurants.json')
     const fileData  = fs.readFileSync(filePath)
     //동기화해서 불러온다는 개념이 중요!!
@@ -64,5 +81,16 @@ app.get('/recommend',function(req,res){
     res.render('index');
  });
 
+//미들웨어 라우팅 핸들링 
+app.use(function(req,res){
+    res.status(404).render('404');
+//url잘못 입력했을 때 에러페이지 핸들링
+});
+
+app.use(function(error, req, res, next){
+    res.status(500).render('500');
+    //서버측 에러 핸들링
+    //개발자 도구에서 확인 할 수 있도록 상태표시
+});
 
 app.listen(3000);
